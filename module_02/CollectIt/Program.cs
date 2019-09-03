00f5544274ddd62a6af6e065ca1ceedaf89ac75c
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-03 15:52:26 +0300

    Completed unit 2.3 - Queue Things

--- module_02/CollectIt/Program.cs
+++ module_02/CollectIt/Program.cs
@@ -7,20 +7,37 @@ namespace CollectIt
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int>(10);
-            var capacity = -1;
+            // Unit 2.2 - LIST<T>
 
-            while (true)
+            //var numbers = new List<int>(10);
+            //var capacity = -1;
+
+            //while (true)
+            //{
+            //    if (numbers.Capacity != capacity)
+            //    {
+            //        capacity = numbers.Capacity;
+            //        Console.WriteLine(capacity);
+            //    }
+
+            //    numbers.Add(1);
+            //}
+
+
+
+            // Unit 2.3 - QUEUE<T>
+
+            Queue<Employee> line = new Queue<Employee>();
+            line.Enqueue(new Employee { Name  = "Alex" });
+            line.Enqueue(new Employee { Name  = "Paul" });
+            line.Enqueue(new Employee { Name  = "Chris" });
+
+            while (line.Count > 0)
             {
-                if (numbers.Capacity != capacity)
-                {
-                    capacity = numbers.Capacity;
-                    Console.WriteLine(capacity);
-                }
-
-                numbers.Add(1);
+                var employee = line.Dequeue();
+                Console.WriteLine(employee.Name);
             }
-
+            
         }
     }
 }
