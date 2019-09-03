2f3f98b62fd6ea3ed67478c34c37bf7d52f3897d
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-03 15:33:24 +0300

    Completed unit 2.2 - List Things

---
+++ module_02/CollectIt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>(10);
+            var capacity = -1;
+
+            while (true)
+            {
+                if (numbers.Capacity != capacity)
+                {
+                    capacity = numbers.Capacity;
+                    Console.WriteLine(capacity);
+                }
+
+                numbers.Add(1);
+            }
+
+        }
+    }
+}
