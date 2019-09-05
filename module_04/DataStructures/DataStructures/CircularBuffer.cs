561e06a6e2632c31e788e086c9c241c55f206a4f
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-05 17:13:32 +0300

    Completed unit 4.2 - Generic Methods

---
+++ module_04/DataStructures/DataStructures/CircularBuffer.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructures
+{
+    public class CircularBuffer<T> : Buffer<T>
+    {
+        private int _capacity;
+        public CircularBuffer()
+            : this(capacity: 10)
+        {
+        }
+
+        public CircularBuffer(int capacity)
+        {
+            _capacity = capacity;
+        }
+
+        public override void Write(T value)
+        {
+            base.Write(value);
+            if (_queue.Count > _capacity)
+            {
+                _queue.Dequeue();
+            }
+        }
+
+        public bool IsFull
+        {
+            get { return _queue.Count == _capacity; }
+        }
+    }
+}
