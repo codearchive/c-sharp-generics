d41c3c9bd96492010f9a587eac59eb97d4847d7b
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-10 12:27:20 +0300

    Completed unit 7.3 - The Math Problem

--- module_07/module_07/Program.cs
+++ module_07/module_07/Program.cs
@@ -1,33 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace module_07
 {
-    public static class StringExtensions
-    {
-        public static TEnum ParseEnum<TEnum>(this string value) where TEnum: System.Enum
-        {
-            return (TEnum)Enum.Parse(typeof(TEnum), value);
-        }
-    }
-
-    public enum Steps
-    {
-        Step1,
-        Step2,
-        Step3
-    }
-
-    class Program
+    public class Program
     {
         static void Main()
         {
-            var input = "Step1";
-            var value = input.ParseEnum<Steps>();
-            Console.WriteLine(value.GetType().Name);
+            //var input = "Step1";
+            //var value = input.ParseEnum<Steps>();
+            //Console.WriteLine(value.GetType().Name);
+
+            var numbers = new double[] {1, 2, 3, 4, 5, 6};
+            var result = SampledAverage(numbers);
+            Console.WriteLine(result);
+        }
+
+        private static double SampledAverage(double[] numbers)
+        {
+            var count = 0;
+            var sum = 0.0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+                count += 1;
+            }
+
+            return sum / count;
         }
     }
 }
---
+++ module_07/module_07/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace module_07
+{
+    public enum Steps
+    {
+        Step1,
+        Step2,
+        Step3
+    }
+
+    public static class StringExtensions
+    {
+        public static TEnum ParseEnum<TEnum>(this string value) where TEnum : System.Enum
+        {
+            return (TEnum)Enum.Parse(typeof(TEnum), value);
+        }
+    }
+}
