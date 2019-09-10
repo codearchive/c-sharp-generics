af92b30b5dfc3b4715f1d7a4b13fcb00ad02e26c
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-10 13:31:45 +0300

    Completed unit 7.5 - Generics and Statics

---
+++ module_07/module_07/InstanceCount.cs
@@ -0,0 +1,17 @@
+﻿namespace module_07
+{
+    public class Item<T> : Item
+    {
+
+    }
+
+    public class Item
+    {
+        public static int InstanceCount;
+
+        public Item()
+        {
+            InstanceCount += 1;
+        }
+    }
+}
--- module_07/module_07/Program.cs
+++ module_07/module_07/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace module_07
 {
@@ -6,27 +6,41 @@ namespace module_07
     {
         static void Main()
         {
+            // Unit 7.2 - Generics and Enums
+
             //var input = "Step1";
             //var value = input.ParseEnum<Steps>();
             //Console.WriteLine(value.GetType().Name);
+
+
+
+            // Unit 7.3 - The Math Problem
 
             //var numbers = new double[] { 1, 2, 3, 4, 5, 6 };
             //var result = SampledAverage(numbers);
             //Console.WriteLine(result);
 
-            var list = new List<Item>();
 
-            list.Add(new Item<int>());
+
+            // Unit 7.4 - Using Base Types
+
+            //var list = new List<Item>();
+
+            //list.Add(new Item<int>());
+            //list.Add(new Item<double>());
+
+
+
+            // Unit 7.5 - Generics and Statics
+
+            var a = new Item<int>();
+            var b = new Item<int>();
+            var c = new Item<string>();
+
+            Console.WriteLine(Item.InstanceCount);
+            Console.WriteLine(Item.InstanceCount);
         }
     }
 
-    public class Item<T> : Item
-    {
 
-    }
-
-    public class Item
-    {
-
-    }
 }
