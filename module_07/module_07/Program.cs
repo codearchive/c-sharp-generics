066f444184bec3b70b45b2e1c9e420b67063b130
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-10 11:57:00 +0300

    Completed unit 7.2 - Generics and Enums

---
+++ module_07/module_07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_07
+{
+    public static class StringExtensions
+    {
+        public static TEnum ParseEnum<TEnum>(this string value) where TEnum: System.Enum
+        {
+            return (TEnum)Enum.Parse(typeof(TEnum), value);
+        }
+    }
+
+    public enum Steps
+    {
+        Step1,
+        Step2,
+        Step3
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var input = "Step1";
+            var value = input.ParseEnum<Steps>();
+            Console.WriteLine(value.GetType().Name);
+        }
+    }
+}
